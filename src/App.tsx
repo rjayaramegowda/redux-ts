d0757fe78f7bf6cf99f0e9e85deebc9bba8bce29
Ravichandran Jayaramegowda <50406692+rjayaramegowda@users.noreply.github.com>
2022-01-23 20:15:22 +0530

    added google map autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import { ChangeEvent } from "react";
+import usePlacesAutocomplete from "use-places-autocomplete";
 
-function App() {
+export default function App() {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+  } = usePlacesAutocomplete();
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>): void => {
+    setValue(e.target.value);
+  };
+
+  function renderList(): JSX.Element {
+    const suggestions = data.map(({ place_id, description }: any) => (
+      <li key={place_id}>{description}</li>
+    ));
+
+    return <ul>{suggestions}</ul>;
+  }
+
   return (
-    <div className="container mt-5">
-      <h1>Welcome</h1>
+    <div className="conatiner">
+      <input
+        style={{ width: 300, maxWidth: "90%" }}
+        value={value}
+        onChange={handleInput}
+        disabled={!ready}
+      />
+      {renderList()}
     </div>
   );
 }
-
-export default App;
