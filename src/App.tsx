27ff6e7b1b11c5b97b1bffcc2e926d2b77a7c83b
Ravichandran Jayaramegowda <50406692+rjayaramegowda@users.noreply.github.com>
2022-01-25 00:56:42 +0530

    axios typescript datatypes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState<Post[] | null>(null);
+  useEffect(() => {
+    console.log("init()");
+    getData();
+  }, []);
+
+  async function getData() {
+    console.log("init()");
+    const { data } = await axios.get<Post[]>(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    setPosts(data);
+    // console.log(data);
+  }
+
   return (
-    <div className="container mt-5">
-      <h1>Welcome</h1>
+    <div>
+      {posts?.map((post: Post) => (
+        <li>{post.title}</li>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
